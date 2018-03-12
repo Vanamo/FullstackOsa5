e966361d03e58fc59d06e4ed7b574ec7b8dc3e3e
Piirainen Vanamo K <vseppane@da3-cs-dk110-13.pc.helsinki.fi>
2018-03-12 13:45:40 +0200

    pieniä korjauksia

--- src/App.js
+++ src/App.js
@@ -75,6 +75,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log('render')
 
     const userById = (id) => {
       console.log('allUsers', this.props.allUsers)
--- src/index.js
+++ src/index.js
@@ -4,14 +4,10 @@ import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
 
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('root')
-  )
-}
 
-render()
-store.subscribe(render)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
